4a7ca910c161bfdae89d55f42960355177ce7414
IvoCobos <36011652+IvoCobos@users.noreply.github.com>
2018-06-04 13:59:30 -0500

    Add files via upload

---
+++ calculadora.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Claculadora
+{
+    public class Claculadora
+    {
+        public void double Sumar(double sumando1, double sumando2)
+        {
+            return sumando1+sumando2;
+        }
+        public void double Restar(double minuendo, double sustraendo)
+        {
+            return minuendo-sustraendo;
+        }
+        public void double Multiplicar(double multiplicando, double multiplicador)
+        {
+            return multiplicando*multiplicador;
+        }
+        public void double Dividir(double dividendo, double divisor)
+        {
+            return dividendo/divisor;
+        }
+        public void double Negacion(double residuo, double cocientes)
+        {
+
+        }
+        public void double 
+    }
+}
---
+++ objeto.cs
@@ -0,0 +1,8 @@
+double valor1=5;
+var miCalcu=new Calculadora();
+var result=miCalcu.sumar(valor1,2);
+var result=miCalcu.restar(valor1,3);
+var result=miCalcu.raiz(result);
+var result=miCalcu.dividir(result/8);
+var result=miCalcu.multiplicar(result,4);
+console.WriteLine("El resultado es: "+result);
